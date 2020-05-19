9ed0b4498c85f785aa0f9d10f0ca05a4aaba21fd
Jin Choi <jinchoi@ucdavis.edu>
2020-05-19 12:12:18 -0700

    added tasks post endpoint

--- src/index.js
+++ src/index.js
@@ -12,12 +12,48 @@ app.post('/users', (req, res) => {
 
   newUser.save()
     .then(() => {
-      res.send(newUser);
+      res.status(201).send(newUser);
     })
     .catch(err => {
       res.status(400).send(err);
     })
 });
+
+app.get('/users', (req, res) => {
+  User.find({})
+    .then(users => {
+      res.send(users);
+    })
+    .catch(err => {
+      res.status(500).send();
+    });
+});
+
+app.get('/users/:id', (req, res) => {
+  User.findById(req.params.id)
+    .then(user => {
+      if (!user) {
+        return res.send(404);
+      }
+
+      res.send(user);
+    })
+    .catch(e => {
+      res.status(500).send(e);
+    });
+});
+
+app.post('/tasks', (req, res) => {
+  const newTask = new Task(req.body);
+
+  newTask.save()
+    .then(() => {
+      res.status(201).send(newTask);
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    })
+})
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
