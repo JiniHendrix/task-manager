b104a4a93807c69809092b0fbfd4df02c6236f73
Jin Choi <jinchoi@ucdavis.edu>
2020-05-20 16:02:41 -0700

    added logout endpoint

--- src/routers/user.js
+++ src/routers/user.js
@@ -15,6 +15,17 @@ router.post('/users', async (req, res) => {
   }
 });
 
+router.post('/users/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter(token => token.token !== req.token);
+    await req.user.save();
+
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
 router.get('/users/me', auth, (req, res) => {
   res.send(req.user);
 });
