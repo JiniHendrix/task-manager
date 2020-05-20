8e5cd1aca96ccc06607b884a693eb1b5b271e997
Jin Choi <jinchoi@ucdavis.edu>
2020-05-19 19:56:32 -0700

    moved task routes to its own file

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
-const { User, Task } = require('./models');
-const { userRouter } = require('./routers');
+const { userRouter, taskRouter } = require('./routers');
 require('./db/mongoose');
 
 const app = express();
@@ -8,77 +7,7 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(userRouter);
-
-app.post('/tasks', (req, res) => {
-  const newTask = new Task(req.body);
-
-  newTask.save()
-    .then(() => {
-      res.status(201).send(newTask);
-    })
-    .catch(err => {
-      res.status(400).send(err);
-    })
-});
-
-app.get('/tasks', async (req, res) => {
-  try {
-    const tasks = await Task.find({});
-
-    res.send(tasks);
-  } catch (e) {
-    res.send(500);
-  }
-});
-
-app.get('/tasks/:id', async (req, res) => {
-  try {
-    const task = await Task.findById({ _id: req.params.id });
-    if (!task) {
-      return res.send(404);
-    }
-  
-    res.send(task);
-  } catch (e) {
-    res.send(500);
-  }
-});
-
-app.patch('/tasks/:id', async (req, res) => {
-  const allowedUpdates = ['description', 'completed'];
-  const proposedUpdates = Object.keys(req.body);
-  const isValid = proposedUpdates.every(proposedUpdate => allowedUpdates.includes(proposedUpdate));
-
-  if (!isValid) {
-    return res.status(404).send('Invalid fields present');
-  }
-
-  try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-
-    if (!task) {
-      return res.send(404);
-    }
-
-    res.send(task);
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
-
-app.delete('/tasks/:id', async (req, res) => {
-  try {
-    const task = await Task.findByIdAndDelete(req.params.id);
-
-    if (!task) {
-      return res.send(404);
-    }
-
-    res.send(task)
-  } catch (e) {
-    res.status(400).send(e);
-  }
-});
+app.use(taskRouter);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,5 +1,7 @@
 const userRouter = require('./user');
+const taskRouter = require('./task');
 
 module.exports = {
   userRouter,
+  taskRouter,
 };
