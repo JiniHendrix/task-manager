b45b740651e961ce03acb97f294ea59ab5d1f3a7
Jin Choi <jinchoi@ucdavis.edu>
2020-05-21 12:25:11 -0700

    added pagination to read tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -19,6 +19,8 @@ router.post('/tasks', auth, async (req, res) => {
   }
 });
 
+// GET /tasks?limit=10&skip20
+
 router.get('/tasks', auth, async (req, res) => {
   const match = {};
 
@@ -30,6 +32,10 @@ router.get('/tasks', auth, async (req, res) => {
     await req.user.populate({
       path: 'tasks',
       match,
+      options: {
+        limit: parseInt(req.query.limit),
+        skip: parseInt(req.query.skip),
+      }
     }).execPopulate();
 
     res.send(req.user.tasks);
