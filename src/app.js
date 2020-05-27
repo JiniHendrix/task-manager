20ceb000b290ee7b8811d130e3557f588ae7c4df
Jin Choi <jinchoi@ucdavis.edu>
2020-05-27 00:11:42 -0700

    added jest and supertest and moved app to its own file for testing

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const { userRouter, taskRouter } = require('./routers');
+const sgMail = require('@sendgrid/mail');
+require('./db/mongoose');
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
+
+module.exports = app;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,5 @@
-const express = require('express');
-const { userRouter, taskRouter } = require('./routers');
-const sgMail = require('@sendgrid/mail');
-require('./db/mongoose');
-
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-const app = express();
+const app = require('./app');
 const port = process.env.PORT || 3000;
-
-app.use(express.json());
-app.use(userRouter);
-app.use(taskRouter);
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
