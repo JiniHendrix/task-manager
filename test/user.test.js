16861913cc72ce307c3d8b599b81239d2dac4ec2
Jin Choi <jinchoi@ucdavis.edu>
2020-05-27 16:43:59 -0700

    updated delete and login tests to test data instead of status codes

--- test/user.test.js
+++ test/user.test.js
@@ -33,11 +33,13 @@ test('Should signup a new user', async () => {
 });
 
 test('Should login existing user', async () => {
-  await request(app).post('/users/login').send({
+  const response = await request(app).post('/users/login').send({
     email: 'jorge@gmail.com',
     password: '1234abcd',
     name: 'Jorge'
-  }).expect(200)
+  })
+  const user = await User.findById(userOneId);
+  expect(response.body.token).toBe(user.tokens[1].token)  
 })
 
 test('Should not login nonexisting user', async () => {
@@ -63,11 +65,13 @@ test('Should error getting profile for unauthenticated user', async () => {
 })
 
 test('Should delete account for user', async () => {
-  await request(app)
+  const response = await request(app)
     .delete('/users/me')
     .set('Authorization', `Bearer ${userOne.tokens[0].token}`)
     .send()
-    .expect(200)
+  
+  const user = await User.findById(userOneId)
+  expect(user).toBeNull()
 })
 
 test('Should not delete account for unauthenticated user', async () => {
